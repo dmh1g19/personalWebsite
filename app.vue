8c50b2102a164387ad2e0fc2972b28758f78c29b
dmh1g19 <dannymartinezhibbert@gmail.com>
2024-11-20 18:46:50 +0000

    Reponsive design

--- app.vue
+++ app.vue
@@ -3,3 +3,13 @@
     <NuxtPage />
   </div>
 </template>
+
+<script setup>
+definePageMeta({
+  head: {
+    meta: [
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' }
+    ]
+  }
+});
+</script>
